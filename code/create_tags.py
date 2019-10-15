736505beadaf43baa8414d97e6f938d16502696f
Gianmarco Bernasconi <gibernas@ethz.ch>
2019-10-15 16:27:08 +0200

    Working now

--- code/create_tags.py
+++ code/create_tags.py
@@ -1,173 +1,123 @@
 ## This file creates Apriltags as output
 from shutil import copyfile
 from datetime import datetime
-import yaml
-
-#USER INPUT##################################################
-start=1 #min 1
-end = 99 #max 551
+from utils.creator_utils import load_file
+import codecs
+import argparse
 
 
-## code only works with this setting atm
-withpicture = True
-if withpicture:
-    tagsppage = 3
-else:
-    tagsppage = 6
+def main():
+    # Parse user input TODO: read from file
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--start", type=int, default=1, help="Tag number to start with, min is 1")
+    parser.add_argument("--end", type=int, default=6, help="Tag number to end with, max is 511")
+    parser.add_argument("--no_pic", action="store_true", default="False", help="Remove picture")
+    parser.add_argument("--id_text", default="Tag ID", help="String written under Apriltag")
+    parser.add_argument("--offset", type=int, default=1,
+                        help="Offset in numbering, 400 for Autobots, 1 for localization")
+    parser.add_argument("--database_name", default="apriltagsDB", help="Yaml file containing IDs informations")
+    args = parser.parse_args()
 
-IDtext = 'ID =' #TODO change with id
-offset = 1 # Megabot = 400, Localization = 1
+    start = args.start  # min 1
+    end = args.end  # max 551
 
-#GET DATA FROM APRILTAGS DATABASE
-with open("../apriltagsDB.yaml", 'r') as stream:
-    try:
-        apriltagsDB = yaml.load(stream)
-    except yaml.YAMLError as exc:
-        print(exc)
-
-
-
-
-#LATEX TEXTBLOCKS #################################################
-# commands for April Tag Text #NOTE uncomment next line to write a line
-AprilTagIDTextFront= '\n\\newcommand{\\AprilTagIDTextFront}[1]{%parameter 1:ID Number \n \\centering \n	\\Large{\\textbf{'+ IDtext +' #1}}\\\\ \n	%\\small{\\today} \n	\\vspace*{2mm}\\vspace{50mm} \n}\n'
-AprilTagIDNoTextFront= '\n\\newcommand{\\AprilTagIDNoTextFront}{ \n \\centering \n	\\Large{\\textbf{\\textcolor{white}{simple} }}\\\\ \n	%\\small{\\today} \n	\\vspace*{2mm}\\vspace{50mm} \n}\n'
-
-#AprilTagIDTextFront= '\n\\newcommand{\\AprilTagIDTextFront}[1]{  \n \\centering \n	\\Large{\\textbf{ }}\\\\ \n	%\\small{\\today} \n	\\vspace*{2mm}\\vspace{50mm} \n}\n'
-
-
-
-AprilTagIDText ='\n\\newcommand{\\AprilTagIDText}[1]{THISISME\n	\\centering\n	\\rotatebox[origin=c]{180}{\\huge{'+"HEYTHERE" +'#1}}\\\\\n	\\rotatebox[origin=c]{180}{\\small{\\today}}\n	\\vspace{60mm}\n}\n'
-
-
-# start new page (front)
-new_page = '\\clearpage\n\n\\begin{table}[p]\n	\\centering\n	\\begin{tabular}{m{15mm}m{80mm}m{80mm}m{80mm}m{15mm}}\n		%CutMarkRow\n		\\multicolumn{1}{c|}{}	& \\multicolumn{1}{c|}{\\CutMarkCell{}} & \\multicolumn{1}{c|}{} &        \\multicolumn{1}{c|}{}              &  \\\\ \\cline{1-1} \\cline{5-5}\n\n		%Distance Row\n		& \\TopBottomCell{}  & & & \\\\\n\n'
-# Apriltag row:
-#1
-april1 = '\n%April Tag\n		& \\AprilTagCell{'
-#2
-april2 = '}}  & \\AprilTagCell{'
-#3
-april3 = '}}  & \\AprilTagCell{'
-#4
-april4 = '}} &  \\\\\n'
-
-trafficsign = '\n%April Tag\n		& \\AprilTagCell{\\TrafficSignPicture{'
-
-# text row
-#1
-
-#text1 = '\n%Text Row\n		& \\BetweenCell{} \\AprilTagIDTextFront{'
-text1 = '\n%Text Row\n		& \\AprilTagIDTextFront{'
-notext1 = '\n%Text Row\n		& \\AprilTagIDNoTextFront{'
-#2
-text2 = '} & \\AprilTagIDTextFront{'
-notext2 = '} & \\AprilTagIDNoTextFront{'
-#3
-text3 ='}& \\AprilTagIDTextFront{'
-notext3 ='}& \\AprilTagIDNoTextFront{'
-#4
-text4 ='}& \\\\	\\cline{1-1} \\cline{5-5}\n'
-
-# Distance Row
-distance_row = '\n		%Distance Row\n		& \TopBottomCell{}  & & & \\\\\n'
-
-
-
-# end page
-end_page = '\n		%Distance Row\n		& \\TopBottomCell{}  & & & \\\\\n		%CutMarkRow\n		\\multicolumn{1}{c|}{}	& \\multicolumn{1}{c|}{\\CutMarkCell{}} & \\multicolumn{1}{m{80mm}|}{} &        \\multicolumn{1}{c|}{}              &  \n\n	\\end{tabular}\n\\end{table}\n'
-
-#END LATEX TEXTBLOCKS #################################################
-
-
-
-def create_tags(tag1,tag2,tag3,description1, description2, description3,text):
-    if type(tag1) != int:
-        t1 = "\\Picture{"+str(tag1)+".png"
+    # Code only works with this setting atm
+    withpicture = not args.no_pic
+    if withpicture:
+        tagsppage = 3
     else:
-        t1 = "\\AprilTagPicture{"+str(int(tag1))
+        tagsppage = 6
+
+    id_text = args.id_text
+    offset = 1  # Autobots = 400, Localization = 1
+
+    # Load Apriltags database
+    apriltags_db = load_file('config/' + args.database_name)
+
+    # Load latex strings
+    t_str = load_file('latex/latex_strings')
+    t_str['AprilTagIDTextFront'] = t_str['AprilTagIDTextFront_pre'] + str(id_text) + t_str['AprilTagIDTextFront_post']
+
+    def create_tags(tags=None, text_ids=None, with_text=None):
+        assert tags is not None, "You need to provide some tags!"
+        if with_text:
+            assert text_ids is not None, "If you want to write ids, provide some text!"
+
+        t = []
+        d = []
+        for idx, tag in enumerate(tags):
+            if type(tag) != int:
+                t.append("\\Picture{"+str(tag)+".png")
+            else:
+                t.append("\\AprilTagPicture{"+str(int(tag)))
+                d.append(str(text_ids[idx]))
+
+        string = t_str['april1'] + t[0] + t_str['april2'] + t[1] + t_str['april3'] + t[2] + t_str['april4'] + t_str[
+            'distance_row']
+        if with_text:
+            string += t_str['text1'] + d[0] + t_str['text2'] + d[1] + t_str['text3'] + d[2] + t_str['text4']
+        else:
+            string += t_str['notext1'] + t_str['notext2'] + t_str['notext3'] + t_str['text4']
+
+        return string
+
+    time = "{:%Y%m%d-%H%M}".format(datetime.now())
+    src = 'data/latex/template.tex'
+    filename = 'output/AprilTags_' + time + '.tex'
+    copyfile(src, filename)
+
+    output = open(filename, 'a')
+    output.write(codecs.decode(t_str['AprilTagIDTextFront'], 'unicode_escape'))
+    output.write(codecs.decode(t_str['AprilTagIDNoTextFront'], 'unicode_escape'))
+    output.write(codecs.decode(t_str['AprilTagIDText'], 'unicode_escape'))
+
+    for tag_id in range(start, end+1, tagsppage):
+        tag_types = []
+        traffic_sign_types = []
+        for in_page_idx in range(tagsppage):
+            tag_types.append(apriltags_db[tag_id + in_page_idx]['tag_type'])
+            if apriltags_db[tag_id]['traffic_sign_type'] is not None:
+                traffic_sign_types.append(apriltags_db[tag_id]['traffic_sign_type'])
+            else:
+                traffic_sign_types[in_page_idx].append('empty')
+
+        # Write either 6 apriltags or 3 apriltags and 3 pictures on the front page
+        # TODO: add possibility to specify single ids
+        tag_ids = list(range(tag_id, tag_id + tagsppage))
+        output.write(codecs.decode(t_str['new_page'], 'unicode_escape'))
+        if withpicture:
+            output.write(codecs.decode(create_tags(tags=traffic_sign_types, with_text=False), 'unicode_escape'))
+            output.write(codecs.decode(t_str['distance_row'], 'unicode_escape'))
+            output.write(codecs.decode(create_tags(tags=tag_ids, with_text=False), 'unicode_escape'))
+        else:
+            output.write(codecs.decode(create_tags(tags=tag_ids[0:3],
+                                     text_ids=[_id - offset for _id in tag_ids[0:3]],
+                                     with_text=True), 'unicode_escape'))
+            output.write(codecs.decode(t_str['distance_row'], 'unicode_escape'))
+            output.write(codecs.decode(create_tags(tags=tag_ids[3:],
+                                     text_ids=[_id - offset for _id in tag_ids[3:]],
+                                     with_text=True), 'unicode_escape'))
+
+        output.write(codecs.decode(t_str['end_page'], 'unicode_escape'))
+
+        # This writes the IDs on the next page, either 6 or 3
+        output.write(codecs.decode(t_str['new_page'], 'unicode_escape'))
+        if withpicture:
+            output.write(codecs.decode(create_tags(tags=['empty']*3, with_text=False), 'unicode_escape'))
+            output.write(codecs.decode(t_str['distance_row'], 'unicode_escape'))
+            output.write(codecs.decode(create_tags(tags=['empty']*3,
+                                     text_ids=[_id - offset for _id in tag_ids[0:3]],
+                                     with_text=True), 'unicode_escape'))
+        else:
+            output.write(codecs.decode(create_tags(tags=['empty']*3, with_text=False), 'unicode_escape'))
+            output.write(codecs.decode(t_str['distance_row'], 'unicode_escape'))
+            output.write(codecs.decode(create_tags(tags=['empty']*3, with_text=False), 'unicode_escape'))
+
+        output.write(codecs.decode(t_str['end_page'], 'unicode_escape'))
+
+    output.write(codecs.decode('\\end{document}', 'unicode_escape'))
+    output.close()
 
 
-    if type(tag2) != int:
-        t2 = "\\Picture{"+str(tag2)+".png"
-    else:
-        t2 = '\\AprilTagPicture{'+str(int(tag2))
-
-
-    if type(tag3) != int:
-        t3 = "\\Picture{"+str(tag3)+".png"
-    else:
-        t3 = "\\AprilTagPicture{"+str(int(tag3))
-
-    d1 = str(int(description1))
-    d2 = str(int(description2))
-    d3 = str(int(description3))
-
-    string = april1 + t1 + april2 +t2 +april3 +t3+april4 +distance_row
-    if not withpicture:
-        #string = string + text1 ++ text2 + d2 +text3 +d3 +text4
-        2+2
-
-    else:
-
-        1+1 #TODO fix
-    #if text is True:
-    if text is True:
-        string = string + text1 +d1+ text2 +d2+ text3  +d3+ text4
-    else:
-        string = string + notext1 + notext2  + notext3 + text4
-    return string
-
-time = "{:%Y%m%d-%H%M}".format(datetime.now())
-src = 'template.tex'
-filename = 'AprilTags_' + time + '.tex'
-copyfile(src, filename)
-
-output = open(filename, 'a')
-output.write(AprilTagIDTextFront)
-output.write(AprilTagIDNoTextFront)
-output.write(AprilTagIDText)
-
-# Cuz Apriltags start with Number 0
-start += 1
-end += 1
-
-for tag_id in range(start, end+1,tagsppage):
-#for tag_id in xrange(start, end+1,tagsppage):
-    tag_type1 = apriltagsDB[tag_id]['tag_type']
-    traffic_sign_type1 = apriltagsDB[tag_id]['traffic_sign_type']
-
-    tag_type2 = apriltagsDB[tag_id+1]['tag_type']
-    traffic_sign_type2 = apriltagsDB[tag_id+2]['traffic_sign_type']
-
-    tag_type3 = apriltagsDB[tag_id+2]['tag_type']
-    traffic_sign_type3 = apriltagsDB[tag_id+2]['traffic_sign_type']
-
-    if traffic_sign_type1 is None: traffic_sign_type1="empty"
-    if traffic_sign_type2 is None: traffic_sign_type2="empty"
-    if traffic_sign_type3 is None: traffic_sign_type3="empty"
-
-
-
-
-
-    output.write(new_page)
-    output.write(create_tags(traffic_sign_type1,traffic_sign_type2,traffic_sign_type3,tag_id-offset,tag_id+1-offset,tag_id+2-offset,False))
-    output.write(distance_row)
-    output.write(create_tags(tag_id,tag_id+1,tag_id+2,tag_id-offset,tag_id+1-offset,tag_id+2-offset,False))
-    output.write(end_page)
-
-    #This writes the IDs on the next page
-    output.write(new_page)
-    output.write(create_tags("empty","empty","empty",tag_id-offset,tag_id+1-offset,tag_id+2-offset,False))
-    output.write(distance_row)
-    output.write(create_tags("empty","empty","empty",tag_id-offset,tag_id+1-offset,tag_id+2-offset,True))
-    output.write(end_page)
-
-
-
-
-
-
-output.write('\\end{document}')
-output.close()
+if __name__ == '__main__':
+    main()
--- code/utils/creator_utils.py
+++ code/utils/creator_utils.py
@@ -5,7 +5,7 @@ def load_file(path):
     data = None
     with open('data/' + path + '.yaml', 'r') as stream:
         try:
-            data = yaml.load(stream)
+            data = yaml.safe_load(stream)
         except yaml.YAMLError as exc:
             print(exc)
     return data
