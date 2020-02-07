4078d9cd850b405ceba7a0b49d5d8b6347591ad1
Gianmarco Bernasconi <gibernas@ethz.ch>
2020-02-07 11:41:12 +0100

    Cleanup

--- code/quick_DB_creator.py
+++ code/quick_DB_creator.py
@@ -1,14 +1,15 @@
-def tag_writer(id, tag_type,street_name, vehicle_name, sign_type):
-    yaml.write('- tag_id: ' + str(id) + '\n')
+def tag_writer(id_, tag_type, street_name, vehicle_name, sign_type):
+    yaml.write('- tag_id: ' + str(id_) + '\n')
     yaml.write('  tag_type: ' + tag_type + '\n')
     yaml.write('  street_name: ' + street_name + '\n')
     yaml.write('  vehicle_name: ' + vehicle_name + '\n')
     yaml.write('  traffic_sign_type: ' + sign_type + '\n')
     yaml.write('\n')
 
+
 yaml = open('newDB.yaml', 'w+')
 tag_type = 'Localization'
 start = 300
 end = 400
-for id in xrange(start, end):
-    tag_writer(id, tag_type,'','','')
+for id in range(start, end):
+    tag_writer(id, tag_type, '', '', '')
